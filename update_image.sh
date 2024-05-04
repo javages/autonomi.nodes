fe01ca338346c943944115b456b902c5c32dc26d
Josh <jclaassen79@gmail.com>
2024-05-04 17:02:14 -0400

    new style

---
+++ update_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the path to the local repository
+REPO_PATH="$HOME/autonomi.nodes"
+
+# Define the path to the image on your device
+IMAGE_PATH="$HOME/average_nodes_over_time.png"
+
+# Navigate to the repository directory
+cd $REPO_PATH
+
+# Copy the new image to the repository
+cp $IMAGE_PATH $REPO_PATH/average_nodes_over_time.png
+
+# Add the updated image to the staging area
+git add average_nodes_over_time.png
+
+# Commit the change
+git commit -m "Auto Update"
+
+# Push the commit to GitHub
+git push origin main
