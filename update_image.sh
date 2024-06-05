83843afe9d9ac6a7b718f8b134fcc1c37993cce0
josh <jclaassen79@gmail.com>
2024-06-05 21:07:49 +0000

    Auto Update

--- update_image.sh
+++ update_image.sh
@@ -12,11 +12,8 @@ cd $REPO_PATH
 # Copy the new image to the repository
 cp $IMAGE_PATH $REPO_PATH/average_nodes_over_time.png
 
-# Stash any changes that are not relevant
-git stash --include-untracked
-
 # Add only the updated image to the staging area
-git add average_nodes_over_time.png
+git add .
 
 # Commit the change
 git commit -m "Auto Update"
@@ -24,5 +21,3 @@ git commit -m "Auto Update"
 # Push the commit to GitHub
 git push origin main
 
-# Pop the stashed changes back
-git stash pop
